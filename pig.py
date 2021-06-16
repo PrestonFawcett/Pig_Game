93d6ded188be6b946f14e394406bba07746a2f06
PrestonFawcett <ptfawcett@csu.fullerton.edu>
2021-06-15 21:20:16 -0700

    Changed turn order into a function. Created function for AI roll.

--- pig.py
+++ pig.py
@@ -10,16 +10,59 @@ import time
 
 def die_roll():
     """ Rolls the die """
-    return random.randint(1, 6)
+    value = random.randint(1, 6)
+    time.sleep(1)
+    print('You rolled a {}\n'.format(value))
+    return value
 
 def error():
+    """ Error message for wrong input """
     print('Invalid input, try again.')
+
+def ai_roll(value):
+    """ Computer decision for rolling """
+    time.sleep(1)
+    if value < 20:
+        return 'y'
+    return 'n'
+
+def rearrange(players):
+    """ Rearrange players based on die roll """
+    turn_order = []
+    for i in range(0, len(players)):
+        if players[i].computer:
+            print('\n{} press Enter key to roll for turn order.'.format(
+                players[i].name))
+            time.sleep(1)
+        else:
+            input('\n{} press Enter key to roll for turn order.'.format(
+                players[i].name))
+        players[i].turn_roll = die_roll()
+        #time.sleep(1)
+        #print("You rolled a {}.".format(players[i].turn_roll))
+
+        if i < 1:
+            turn_order.append(players[i])
+        else:
+            found = False
+            for j in range(0, i):
+                if players[i].turn_roll >= turn_order[j].turn_roll:
+                    turn_order.insert(j, players[i])
+                    found = True
+                elif j == i - 1:
+                    turn_order.append(players[i])
+                    found = True
+                if found:
+                    break
+    return turn_order
+
+
 
 def start_turn(player):
     """ Players turn process """
     if player.computer:
         print('Press the Enter key to roll.')
-        time.sleep(2)
+        time.sleep(1)
     else:
         input('Press the Enter key to roll.')
     player.turn_roll = die_roll()
@@ -28,7 +71,6 @@ def start_turn(player):
     pass_die = False
 
     while player.turn_roll != 1 and not pass_die and not player.winner:
-        print('You rolled a {}\n'.format(player.turn_roll))
         current_points = current_points + player.turn_roll
         num_of_rolls = num_of_rolls + 1
 
@@ -42,24 +84,21 @@ def start_turn(player):
             print('Number of rolls:           {}\n'.format(num_of_rolls))
 
             time.sleep(1)
-            if player.computer:
-                print('Do you wish to roll again? (y/n)')
-                time.sleep(2)
-                if current_points < 20:
-                    player.turn_roll = die_roll()
+            answer = ''
+            while answer not in ('y', 'n'):
+                if player.computer:
+                    print('Do you wish to roll again? (y/n)')
+                    answer = ai_roll(current_points)
                 else:
+                    answer = input('Do you wish to roll again? (y/n)')
+
+                if answer == 'y':
+                    player.turn_roll = die_roll()
+                elif answer == 'n':
                     player.scoreboard = player.scoreboard + current_points
                     pass_die = True
-            else:
-                while answer != 'y' and answer != 'n':
-                    answer = input('Do you wish to roll again? (y/n)')
-                    if answer == 'y':
-                        player.turn_roll = die_roll()
-                    elif answer == 'n':
-                        player.scoreboard = player.scoreboard + current_points
-                        pass_die = True
-                    else:
-                        error()
+                else:
+                    error()
 
     if player.turn_roll == 1:
         print('You rolled a 1. Next players turn.')
@@ -132,37 +171,14 @@ def main():
     players = []
     if num_of_players == 2:
         players.append(Player(input('\nEnter your name: '), False))
-        players.append(Player('CPU', True))
+        players.append(Player('John Scarne', True))
     else:
         for i in range(0, num_of_players):
             players.append(Player(
                 input('\nPlayer {} enter your name: '.format(i + 1)), False))
 
     # Creating turn order
-    turn_order = []
-    for i in range(0, num_of_players):
-        if players[i].computer:
-            print('CPU press Enter key to roll for turn order.')
-            time.sleep(2)
-        else:
-            input('\n{} press Enter key to roll for turn order.'.format(players[i].name))
-        players[i].turn_roll = die_roll()
-        time.sleep(1)
-        print("You rolled a {}.".format(players[i].turn_roll))
-
-        if i < 1:
-            turn_order.append(players[i])
-        else:
-            found = False
-            for j in range(0, i):
-                if players[i].turn_roll >= turn_order[j].turn_roll:
-                    turn_order.insert(j, players[i])
-                    found = True
-                elif j == i - 1:
-                    turn_order.append(players[i])
-                    found = True
-                if found:
-                    break
+    turn_order = rearrange(players)
 
     print('\n------Turn Order------')
     for i in range(0, num_of_players):
